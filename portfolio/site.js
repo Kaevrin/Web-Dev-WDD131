a217fa230f40a5f01d3ab4b3ce1b23878f7505da
Kaevrin <159499822+Kaevrin@users.noreply.github.com>
2025-07-19 21:45:04 -0600

    Assembled and populated the pages.

--- portfolio/site.js
+++ portfolio/site.js
@@ -8,7 +8,9 @@ fetch(`https://api.github.com/users/${username}/repos`)
 
         repos.forEach(repo => {
             const section = document.createElement("section");
-            section.className = "project"
+            section.className = "project";
+            const titlecontainer = document.createElement("div");
+            titlecontainer.className = "projectstyle";
 
             const link = document.createElement("a");
             link.href = repo.html_url;
@@ -17,14 +19,28 @@ fetch(`https://api.github.com/users/${username}/repos`)
             const title = document.createElement("h2");
             title.className = "projecttitle";
             title.textContent = repo.name;
-            link.appendChild(title)
+            link.appendChild(title);
+            titlecontainer.appendChild(link)
 
-            const desc = document.createElement("p")
+            const infocontainer = document.createElement("div");
+            infocontainer.className = "infocontainer";
+
+            const desc = document.createElement("p");
             desc.textContent = repo.description || "No description available";
 
-            section.appendChild(link);
-            section.appendChild(desc)
-            container.appendChild(section)
+            const lang = document.createElement("p");
+            if (repo.language && repo.html_url) {
+                lang.innerHTML = `Coded in ${repo.language} - [<a class="projectlink" href="${repo.html_url}" target="_blank">${repo.html_url}</a>]`;
+            } else {
+                lang.textContent = "No language found";
+            }
+
+            infocontainer.appendChild(desc);
+            infocontainer.appendChild(lang);
+
+            section.appendChild(titlecontainer);
+            section.appendChild(infocontainer);
+            container.appendChild(section);
         });
     })
     .catch(error => {
